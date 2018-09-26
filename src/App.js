4b60e57a441fb94af70c0efcc9cddc60803a8764
Nora <norama.matema@gmail.com>
2018-09-26 10:55:47 +0200

    removed leaflet examples, added help text

--- src/App.js
+++ src/App.js
@@ -2,36 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 import WeatherMap from './components/WeatherMap';
-import ExamplesApp from './components/examples/examples-app';
 
 class App extends Component {
-
-  state = {
-    mode: 'weather'
-  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Map Weather and other Leaflet Examples</h1>
+          <h1 className="App-title">Map Weather</h1>
+          <div className="App-help">
+            <div>
+              <div className="App-help-item"><div className="App-help-click">Click on map:</div> place marker</div>
+              <div className="App-help-item"><div className="App-help-click">Click on marker:</div> display weather data</div>
+            </div>
+          </div>
         </header>
-        <p className="App-intro">
-          <a href="#" onClick={this.toggleExamples}>{this.state.mode === 'weather' ? 'Leaflet Examples' : 'Weather'}</a>
-        </p>
-        <br/><br/>
-          
-        {this.state.mode === 'weather' ? <WeatherMap /> : <ExamplesApp />}
-        
+        <WeatherMap />
       </div>
     );
   }
-
-  toggleExamples = () => {
-    this.setState((prevState) => ({
-        mode: prevState.mode === 'weather' ? 'examples' : 'weather'
-      }));
-  };
 }
 
 export default App;
