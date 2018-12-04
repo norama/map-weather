93ce7aa041cdb1516c0a52e19d1d268429fb0dcf
Unknown <norama.matema@gmail.com>
2018-12-04 13:10:16 +0100

    update: title text

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
           <h1 className="App-title">Map Weather</h1>
           <div className="App-help">
             <div>
-              <div className="App-help-item"><div className="App-help-click">Click on map:</div> place marker</div>
-              <div className="App-help-item"><div className="App-help-click">Click on marker:</div> display weather data</div>
+              <div className="App-help-item"><div className="App-help-click">Click on map / search location:</div> place marker with weather data</div>
+              <div className="App-help-item"><div className="App-help-click">Drag marker:</div> display weather data at modified location</div>
             </div>
           </div>
         </header>
