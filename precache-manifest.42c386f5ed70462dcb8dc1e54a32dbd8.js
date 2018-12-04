501203fa1cc33dc4c17db4bf8801ead644fd75ec
Nora Mate <norama.matema@gmail.com>
2018-12-04 13:23:00 +0100

    Updates

---
+++ precache-manifest.42c386f5ed70462dcb8dc1e54a32dbd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7b7333c8ffe2e790526",
+    "url": "/map-weather/static/js/runtime~main.f7b7333c.js"
+  },
+  {
+    "revision": "17adc03c2621bc363ec2",
+    "url": "/map-weather/static/js/main.17adc03c.chunk.js"
+  },
+  {
+    "revision": "e4134575b138d6c2c76b",
+    "url": "/map-weather/static/js/1.e4134575.chunk.js"
+  },
+  {
+    "revision": "17adc03c2621bc363ec2",
+    "url": "/map-weather/static/css/main.fdd2c24e.chunk.css"
+  },
+  {
+    "revision": "e4134575b138d6c2c76b",
+    "url": "/map-weather/static/css/1.f44031f9.chunk.css"
+  },
+  {
+    "revision": "611191d6070109a6d603f34cdb99bb32",
+    "url": "/map-weather/index.html"
+  }
+];
